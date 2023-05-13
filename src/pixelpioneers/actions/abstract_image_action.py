f46243d193fdacb6d14dfe133c3f4e8e047164ac
ameyk <kolheamey99@gmail.com>
2023-05-13 15:55:24 -0400

    abstract base class implementation

---
+++ src/pixelpioneers/actions/abstract_image_action.py
@@ -0,0 +1,13 @@
+from abc import ABC
+
+import numpy as np
+
+
+class AbstractImageAction(ABC):
+    name = "AbstractImageAction"
+
+    def __init__(self):
+        pass
+
+    def apply(self, image: np.ndarray, *args, **kwargs) -> np.ndarray:
+        pass
---
+++ src/pixelpioneers/actions/adjustments/__init__.py
@@ -0,0 +1,9 @@
+from ._brightness_adjustment import BrightnessAdjustment
+from ._contrast_adjustment import ContrastAdjustment
+from ._saturation_adjustment import SaturationAdjustment
+
+__all__ = [
+    "ContrastAdjustment",
+    "BrightnessAdjustment",
+    "SaturationAdjustment"
+]
