0a9b0e6f4292d323c816b417e6302011c8afdffd
ameyk <kolheamey99@gmail.com>
2023-05-15 22:35:04 -0400

    added CLI parser and log handler: - verbosity - debug mode

--- src/pixelpioneers/cli.py
+++ src/pixelpioneers/cli.py
@@ -1,6 +1,30 @@
+import argparse
+import logging
+
+parser = argparse.ArgumentParser()
+
+logging_arg_group = parser.add_mutually_exclusive_group(required=False)
+logging_arg_group.add_argument(
+    '-d', '--debug',
+    help="Print lots of debugging statements",
+    action="store_const", dest="loglevel", const=logging.DEBUG,
+    default=logging.WARNING,
+)
+logging_arg_group.add_argument(
+    '-v', '--verbose',
+    help="Be verbose",
+    action="store_const", dest="loglevel", const=logging.INFO,
+)
+
+args = parser.parse_args()
+
+logging.basicConfig(level=args.loglevel)
+logger = logging.getLogger(__name__)
+
+
 def main():
-    print("In CLI: main()")
+    logger.info("In CLI: main()")
 
 
 if __name__ == "__main__":
-    main()
+    main(args)
