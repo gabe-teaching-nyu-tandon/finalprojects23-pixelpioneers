57fcd2e9f8457f565750072de21721146e228140
khuranayashika31 <51834607+khuranayashika31@users.noreply.github.com>
2023-05-17 16:20:31 -0400

    fixed errors

--- src/pixelpioneers/image_io/_bmp_io.py
+++ src/pixelpioneers/image_io/_bmp_io.py
@@ -6,17 +6,12 @@ from PIL import Image, UnidentifiedImageError
 from pixelpioneers.exceptions import ImageIOError
 from pixelpioneers.image_io._abstract_io import AbstractImageReader, AbstractImageWriter
 
-logger = logging.getLogger(__name__)
-
-
-class JPEGHandler(AbstractImageReader, AbstractImageWriter):
-
+class BMPHandler(AbstractImageReader, AbstractImageWriter):
     def read(self, path: str) -> np.ndarray:
-        img = Image.open(path, formats=["jpeg"])
+        img = Image.open(path, formats=["bmp"])
         img_array = np.array(img)
         return img_array
 
-
 if __name__ == "__main__":
-    jpegHandler = JPEGHandler()
-    img = jpegHandler.read("data/sample.jpeg")
+    bmpHandler = BMPHandler()
+    img = bmpHandler.read("data/sample.bmp")
