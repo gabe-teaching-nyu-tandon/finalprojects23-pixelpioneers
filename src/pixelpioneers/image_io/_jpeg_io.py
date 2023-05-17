ac4918cd5ad2ecb0dcf13b5bcb2a3800f723c32e
khuranayashika31 <51834607+khuranayashika31@users.noreply.github.com>
2023-05-17 16:01:36 -0400

    added read function for jpeg type

--- src/pixelpioneers/image_io/_jpeg_io.py
+++ src/pixelpioneers/image_io/_jpeg_io.py
@@ -12,36 +12,11 @@ logger = logging.getLogger(__name__)
 class JPEGHandler(AbstractImageReader, AbstractImageWriter):
 
     def read(self, path: str) -> np.ndarray:
-        try:
-            logger.debug(f"Reading Image from Path -> {path}")
-            img = Image.open(path, formats=["jpeg"])
-            img_array = np.array(img)
-            return img_array
-
-        except FileNotFoundError as fnfe:
-            raise ImageIOError(f"Error reading image: File '{path}' not found.")
-
-        except UnidentifiedImageError as uie:
-            raise ImageIOError(f"Error reading image: Unrecognized file format.")
-
-        except Exception as e:
-            raise ImageIOError
-
-    def write(self, path: str, image: np.ndarray) -> bool:
-        try:
-            logger.debug(f"Writing Image -> {path}")
-            img = Image.fromarray(image)
-            img.save(path)
-            return True
-
-        except IOError as ioe:
-            raise ImageIOError(f"Error writing image: {ioe}")
-
-        except Exception as e:
-            raise ImageIOError
+        img = Image.open(path, formats=["jpeg"])
+        img_array = np.array(img)
+        return img_array
 
 
 if __name__ == "__main__":
     jpegHandler = JPEGHandler()
     img = jpegHandler.read("data/sample.jpeg")
-    jpegHandler.write("data/sample_out.jpeg", img)
