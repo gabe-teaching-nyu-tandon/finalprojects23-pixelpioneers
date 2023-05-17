5a4214b48562c156704c347aa7e0bfb40b2b2ffd
khuranayashika31 <51834607+khuranayashika31@users.noreply.github.com>
2023-05-17 15:32:01 -0400

    added read function for bmp type

---
+++ src/pixelpioneers/image_io/_bmp_io.py
@@ -0,0 +1,21 @@
+import logging
+
+import numpy as np
+from PIL import Image, UnidentifiedImageError
+
+from pixelpioneers.exceptions import ImageIOError
+from pixelpioneers.image_io._abstract_io import AbstractImageReader, AbstractImageWriter
+
+logger = logging.getLogger(__name__)
+
+
+class BMPHandler(AbstractImageReader, AbstractImageWriter):
+
+    def read(self, path: str) -> np.ndarray:
+            img = Image.open(path, formats=["bmp"])
+            img_array = np.array(img)
+            return img_array
+
+if __name__ == "__main__":
+    bmpHandler = BMPHandler()
+    img = bmpHandler.read("data/sample.bmp")
